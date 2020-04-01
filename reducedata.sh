f8de3b1579480b03b9b0cacb8c8a12ed319b5c68
Alex Bokov <bokov@uthscsa.edu>
2020-03-31 21:53:24 -0500

    reducedata.sh; reproducibility; Documented bash commands needed to get rid of codes/lines that break things and sort all the tables so they are the same order

--- reducedata.sh
+++ reducedata.sh
@@ -17,3 +17,8 @@ echo "Creating $outdir/obsfact_efi_small.tsv";
 cut -f 2,5,10 "$indir/obsfact_efi.tsv" > "$outdir/obsfact_efi_small.tsv";
 echo "Creating $outdir/heron_terms_diag_meds.tsv";
 sed -n "1p;/\\\i2b2\\\\\(Medication\|Epic Diagnosis\|Diagnosis\\)\\\/p" "$indir/heron_terms.tsv"|cut -f 1,3,4,5,6,7,13,14|grep -v Diagnosis > "$outdir/heron_terms_diag_meds.tsv";
+
+# There are FURTHER steps before this stuff is usable by CODEHR...
+for ii in $(ls bigdata00/offline_chi/efi*.tsv|head -n 4);do grep -v "^UHSOrd" $ii | grep -v $'^\r' | grep -v "^[[]" > $(basename $ii); done;
+for ii in $(ls efi*.tsv); do head $ii -n 1 > cleaned/$ii; done
+for ii in $(ls efi*.tsv); do tail $ii -n +2 | sort >> cleaned/$ii; done
