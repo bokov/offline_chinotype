7ecbeb68699278b40b2bb90b00667cd57e68df39
Alex Bokov <bokov@uthscsa.edu>
2020-05-05 21:12:51 -0500

    reducedata.sh; feature; Added creation of obsfact_ip.tsv, obsfact_ed.tsv, and obsfact_disc.tsv

--- reducedata.sh
+++ reducedata.sh
@@ -19,6 +19,15 @@ echo "Creating $outdir/heron_terms_diag_meds.tsv";
 sed -n "1p;/\\\i2b2\\\\\(Medication\|Epic Diagnosis\|Diagnosis\\)\\\/p" "$indir/heron_terms.tsv"|cut -f 1,3,4,5,6,7,13,14|grep -v Diagnosis > "$outdir/heron_terms_diag_meds.tsv";
 
 # There are FURTHER steps before this stuff is usable by CODEHR...
-for ii in $(ls bigdata00/offline_chi/efi*.tsv|head -n 4);do grep -v "^UHSOrd" $ii | grep -v $'^\r' | grep -v "^[[]" > $(basename $ii); done;
+for ii in $(ls $indir/offline_chi/efi*.tsv|head -n 4);do grep -v "^UHSOrd" $ii | grep -v $'^\r' | grep -v "^[[]" > $(basename $ii); done;
 for ii in $(ls efi*.tsv); do head $ii -n 1 > cleaned/$ii; done
 for ii in $(ls efi*.tsv); do tail $ii -n +2 | sort >> cleaned/$ii; done
+
+echo "Creating the inpatient encounters in $outdir";
+cut -f 2,3,5 $indir/observation_fact.tsv |head -n 1 > $outdir/obsfact_ip.tsv;
+cut -f 2,3,5 $indir/observation_fact.tsv |grep "ENC.TYPE:IP" >> obsfact_ip.tsv;
+echo "Creating the emergency encounters in $outdir";
+cut -f 2,3,5 $indir/observation_fact.tsv |head -n 1 > $outdir/obsfact_ed.tsv;
+cut -f 2,3,5 $indir/observation_fact.tsv |grep "ENC.TYPE:ED" >> $outdir/obsfact_ed.tsv;
+cut -f 2,3,5 $indir/observation_fact.tsv |head -n 1 > $outdir/obsfact_disc.tsv;
+cut -f 2,3,5 $indir/observation_fact.tsv |grep "DischargeStatus" >> $outdir/obsfact_disc.tsv;
